c0b8a6d053b68d25337df1fb4259f1ef875381d0
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2019-05-10 22:07:11 +0200

    Test mouse raycast on plane

---
+++ Crytivo Application Demos/Assets/Demos/Procedural Generation/CubeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+internal sealed class CubeGenerator : MonoBehaviour
+{
+    private new Camera camera;
+
+    [SerializeField]
+    private float rayMaxDistance = float.PositiveInfinity;
+
+    [SerializeField]
+    private LayerMask rayLayerMask;
+
+    private void Awake()
+    {
+        // Cache reference to camera
+        this.camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        var mouseRay = this.GetMouseRay();
+
+        Debug.DrawRay(mouseRay.origin, mouseRay.direction, Color.yellow);
+
+        if (Physics.Raycast(mouseRay, out RaycastHit hit, this.rayMaxDistance, this.rayLayerMask))
+        {
+            Debug.DrawLine(hit.point, hit.point + Vector3.up, Color.green);
+        }
+    }
+
+    private Ray GetMouseRay()
+    {
+        return this.camera.ScreenPointToRay(Input.mousePosition);
+    }
+}
---
+++ Crytivo Application Demos/Assets/Demos/Procedural Generation/CubeInteractor.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+internal sealed class CubeInteractor : MonoBehaviour
+{
+
+}
