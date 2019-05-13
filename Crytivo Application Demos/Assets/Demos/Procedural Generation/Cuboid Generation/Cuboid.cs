597f6eef2034ce87aa382eea208caafc0400883f
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2019-05-14 00:49:45 +0200

    Make weird-looking cube with only 8 vertices

--- Crytivo Application Demos/Assets/Demos/Procedural Generation/Cuboid Generation/Cuboid.cs
+++ Crytivo Application Demos/Assets/Demos/Procedural Generation/Cuboid Generation/Cuboid.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(MeshFilter))]
 internal sealed class Cuboid : MonoBehaviour
 {
     private Rigidbody rb;
+    private MeshFilter meshFilter;
 
     public bool UsePhysics
     {
@@ -22,5 +23,46 @@ internal sealed class Cuboid : MonoBehaviour
     private void Awake()
     {
         this.rb = this.GetComponent<Rigidbody>();
+        this.meshFilter = this.GetComponent<MeshFilter>();
+    }
+
+    private void Start()
+    {
+        var mesh = this.meshFilter.mesh;
+
+        mesh.Clear();
+
+        mesh.vertices = new Vector3[]
+        {
+            new Vector3(-0.5f, -0.5f, -0.5f),   // CBL 0
+            new Vector3(-0.5f, -0.5f, 0.5f),    // FBL 1
+            new Vector3(0.5f, -0.5f, 0.5f),     // FBR 2
+            new Vector3(0.5f, -0.5f, -0.5f),    // CBR 3
+            new Vector3(-0.5f, 0.5f, -0.5f),    // CTL 4
+            new Vector3(-0.5f, 0.5f, 0.5f),     // FTL 5
+            new Vector3(0.5f, 0.5f, 0.5f),      // FTR 6
+            new Vector3(0.5f, 0.5f, -0.5f),     // CTR 7
+        };
+
+        mesh.triangles = new int[]
+        {
+            2, 1, 0,    // B
+            0, 3, 2,
+            1, 2, 5,    // F
+            2, 6, 5,
+            2, 3, 6,    // R
+            3, 7, 6,
+            3, 0, 4,    // C
+            4, 7, 3,
+            0, 1, 4,    // L
+            1, 5, 4,
+            4, 5, 6,    // T
+            6, 7, 4
+        };
+
+        mesh.Optimize();
+        mesh.RecalculateNormals();
+
+        this.meshFilter.mesh = mesh;
     }
 }
